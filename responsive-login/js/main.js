ad9ac461b0ff61b44f17dc13e9b7edaa160c8658
Diana Regina <dianaregina22@outlook.com.br>
2020-06-19 21:46:44 -0300

    Add files via upload

---
+++ responsive-login/js/main.js
@@ -0,0 +1,19 @@
+/*Código para adicionar a classe focus no input ao clicar em cima do campo no formulário*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add('focus');
+}
+
+function blurFunc(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == "") {
+		parent.classList.remove('focus');
+	}
+}
+
+inputs.forEach(input => {
+	input.addEventListener('focus', focusFunc);
+	input.addEventListener('blur', blurFunc);
+})
